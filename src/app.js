2c1478a4d3619e671ee0b946bcacec9555933923
tmnkakr <tmnkakr@gmail.com>
2022-04-25 22:27:07 +0530

    comit

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import { Routes, Route } from 'react-router-dom'
 import EditorPage from './components/editor/classbased/editorPage'
-// import LoginPageForArticalVerificationPanel from './components/panel/articleverficationpanel/classbased/loginPage'
+import LoginPageForArticalVerificationPanel from './components/panel/articleverficationpanel/classbased/loginPage'
 // import HomePageForArticalVerificationPanel from './components/panel/articleverficationpanel/classbased/homePage'
 // import ArticlePageForArticalVerificationPanel from './components/panel/articleverficationpanel/classbased/articlePage'
-import axios from "axios";
+//import axios from "axios";
 import S from "./components/S";
-axios.defaults.withCredentials = true
+//axios.defaults.withCredentials = true
 class App extends React.Component {
   // state = {
   //   authorised: JSON.parse(localStorage.getItem("authorized")) ?? false,
@@ -32,10 +32,10 @@ class App extends React.Component {
     return (
       <>
         <Routes>
-          <Route path='/' element={<S />} />
-          <Route exact path="/editor" element={<EditorPage />} />
-          {/* <Route path='/panel/articleverification ' element={<LoginPageForArticalVerificationPanel authorizeUser={this.authorizeUser} />} />
-          <Route path='/panel/articleverification/homepage' element={<HomePageForArticalVerificationPanel authorized={this.state.authorised} />} />
+          <Route path="/"> <S /> </Route>
+          <Route path="/editor"> <EditorPage /> </Route>
+          <Route path='/panel/articleverification '> <LoginPageForArticalVerificationPanel authorizeUser={this.authorizeUser} /> </Route>
+          {/* <Route path='/panel/articleverification/homepage' element={<HomePageForArticalVerificationPanel authorized={this.state.authorised} />} />
           <Route path='/panel/articleverification/articlepage' element={<ArticlePageForArticalVerificationPanel authorized={this.state.authorised} adminId={this.state.adminId} />} /> */}
 
         </Routes>
